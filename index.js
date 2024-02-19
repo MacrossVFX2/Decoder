1ee04993835f1cf0d2f681f3cd37be9b6167a058
MacrossVFX2 <macross.vfx@icloud.com>
2024-02-18 18:09:20 -0600

    Intentando el java

---
+++ index.js
@@ -0,0 +1,13 @@
+var btnEncrypt = document.querySelector(".btn-encrypt");
+var btnDeccrypt = document.querySelector(".btn-decrypt");
+var lupa = document.querySelector(".img-lupa");
+var container = document.querySelector(".container");
+var evaluate = document.querySelector(".evaluate");
+
+btnEncrypt.onclick = encrypt;
+btnDeccrypt.onclick = btnDeccrypt
+
+function encrypt(){
+    hideFoward();
+    var 
+}

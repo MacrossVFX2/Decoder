3e70ae5ad3289911a5af32e23b38213137a39ede
MacrossVFX2 <macross.vfx@icloud.com>
2024-02-18 19:13:27 -0600

    No se aplicaban las reglas del Java, Al parecer hay que definir la instruccion por que me envia un error y el codigo no se ejecuta

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-var btnEncrypt = document.querySelector(".btn-encrypt");
-var btnDeccrypt = document.querySelector(".btn-decrypt");
-var lupa = document.querySelector(".img-lupa");
-var container = document.querySelector(".container");
-var evaluate = document.querySelector(".evaluate");
+//----Declarando los elementos para poder trabajarlos. Funciona mejor con constantes que con let o va---//
+const btnEncrypt = document.querySelector(".btn-encrypt");
+const btnDeccrypt = document.querySelector(".bnt-decrypt");
+const encryptText = document.querySelector(".encrypt");
+const warning = document.querySelector(".warning-text");
 
-btnEncrypt.onclick = encrypt;
-btnDeccrypt.onclick = btnDeccrypt
+//--Funcion de boton Encrypt--//
+btnEncrypt.addEventListener("click", e=>{
+    e.preventDefault();
+    let text = encryptText.value;
+    let txt = text.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
+    
+    if(text == ""){
+        warning.style.background = "#0A3871";
+        warning.style.color = "#FFFF";
+        warning.style.fontWeight = "800";
+        warning.textContent = "Error, el campo no puede estar vacio. Favor de corregir";
+        
+        setTimeout(()=>{
+            warning.removeAttribute("style");
+        },1500);
+    }
 
-function encrypt(){
-    hideFoward();
-    var 
-}
+    else if(text !== txt){
+        warning.style.background = "#0A3871";
+        warning.style.color = "#FFFF";
+        warning.style.fontWeight = "800";
+        warning.textContent = "Acentos y caracteres especiales no estan permitidos";
+        
+        setTimeout(()=>{
+            warning.removeAttribute("style");
+        },1500);
+    }
+
+    else if(text !== text.toLowerCase()){
+        warning.style.background = "#0A3871";
+        warning.style.color = "#FFFF";
+        warning.style.fontWeight = "800";
+        warning.textContent = "El texto debe estar solo en minusulas";
+        
+        setTimeout(()=>{
+            warning.removeAttribute("style");
+        },1500);
+    }
+
+});
